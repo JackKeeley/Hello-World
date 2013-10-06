35ad958d1d3c019714678a4b74c5fc7094b2dc88
Jack Keeley <JackKeeley@Jacks-MacBook-Pro.local>
2013-10-06 02:05:06 -0500

    Initial Commit

---
+++ Hello World/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Hello World
+//
+//  Created by Jack Keeley on 10/6/13.
+//  Copyright (c) 2013 Jack Keeley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Hello World/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Hello World
+//
+//  Created by Jack Keeley on 10/6/13.
+//  Copyright (c) 2013 Jack Keeley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
